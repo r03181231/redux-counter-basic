1f775521d0ed8f855baa12e39b6c9f7892f16ba7
HaeRheeNam <4ran0318@gmail.com>
2024-01-28 14:31:07 +0900

    [Feat/#1] 리덕스 세팅

--- src/index.js
+++ src/index.js
@@ -7,17 +7,6 @@ import store from "./redux/config/configStore";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // Provider로 App컴포넌트를 감싸줘.
-  // 그럼 App이라는 컴포넌트가 프로바이더의 지배권 안으로 들어오는 거야.
-  // Provider는 store를 기반으로 지배권을 행사해.
-  // store로 만든 중앙 데이터 관리소를 이 App컴포넌트 하부에서 이 App컴포넌트가
-  // 있는 곳 전체에서 store를 쓸 수 있다는 거야.
-
-  // 그래서 옵션으로 store
-  // export한 store를 임포트해와서 넣어줘야 해.
-  // 주의 : 이상한 경로에서 가져오지말고 configStore에서 가져와서 써야해.
-  // export default로 내보낸 건 import할 때 {}를 빼고 적어줘야해.
-  // export 는 {}를 넣어주고.
   <Provider store={store}>
     <App />
   </Provider>
--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,25 +1,8 @@
 // 중앙 데이터 관리소(store)를 설정하는 부분
-// 두 가지 api가 필요해
-//createStore : store를 만드는 api
-// combineReducers : 리듀서들을 하나로 묶는 역할하는 메서드.
-// 인자로는 변수가 들어가고 객체가 들어갈 거야.
 import { createStore } from "redux";
 import { combineReducers } from "redux";
 
-// rootReducer : reducer들을 하나로 모아서 한 개로 만들어 놓은 기본 reducer를 말해
-// store(중앙state관리소)를 생성해줘서 인자로 리듀서의 묶음(rootReducer)이 들어가야해.
-
-// modules 안에 넣어놓은 state의 묶음들을 객체 형태에 넣어둘 거야.
-// 그럼 웹 애플리케이션 내에서 관리하는 state집단들이
-// rootReducer로 들어가게 돼.
-// 모든 컴포넌트들은 props로 값을 내려주지 않더라도
-// 바로 중앙 데이터 관리소로 데이터를 참조할 수 있어.
 const rootReducer = combineReducers({});
 const store = createStore(rootReducer);
 
-//그리고 이렇게 만든 store를
-// 애플리케이션에 주입해 주는 과정이 필요해.
-// store를 만들었는데 이 store를 사용하고 있지 않아.
-// 이 store를 사용해서 앱 내부로 넣어야 하니까
-// 만든 걸 바깥으로 내보내야 해.
 export default store;
